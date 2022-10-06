76383a19494d538eeee069d9cd4dac9466522001
Bocanel Valerica <79649333+valyempire@users.noreply.github.com>
2022-10-06 18:18:28 +0300

    Am eliminat 'brake' din functia cu switch

--- BocanelValerica/teme/tema1-JavaScript/script.js
+++ BocanelValerica/teme/tema1-JavaScript/script.js
@@ -84,26 +84,18 @@ function caseInSwitch(val) {
   switch (val) {
     case 1:
       return "Luni";
-      break;
     case 2:
       return "Marti";
-      break;
     case 3:
       return "Miercuri";
-      break;
     case 4:
       return "Joi";
-      break;
     case 5:
       return "Vineri";
-      break;
     case 6:
       return "Sambata";
-      break;
     case 7:
       return "Duminica";
-      break;
-    
   }
 }
 
