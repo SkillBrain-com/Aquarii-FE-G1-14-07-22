81d67a662169575b44eef17a3ed3c56fe9af1341
Eric Puskas <eric.puskas@yahoo.com>
2022-10-18 20:38:57 +0300

    3.8 JS Async retro and recap

---
+++ EricPuskas/retro/recap-js/script.js
@@ -0,0 +1,51 @@
+let users = [
+  { id: 0, name: "Mike", age: 20, isDeveloper: true },
+  { id: 1, name: "John", age: 30, isDeveloper: false },
+  { id: 2, name: "Sara", age: 24, isDeveloper: true },
+  { id: 3, name: "Sandra", age: 19, isDeveloper: false }
+];
+
+// find
+// getUserById(2)
+
+const getUserById = (users, id) => {
+  const result = users.find((user) => user.id === id);
+  console.log(result);
+  return result;
+};
+
+// filter
+// getDevelopers()
+const getDevelopers = () => {
+  const developers = users.filter((user) => user.isDeveloper);
+  console.log(developers);
+};
+getDevelopers();
+
+// map
+// addGreetingToUsers(users);  const someUser = getUserById(1);  someUser.greet(); // Hello, my name is Mike
+
+const addGreetingToUsers = (users) => {
+  users = users.map((user) => {
+    const greet = () => {
+      console.log(`Hello, my name is ${user.name}`);
+    };
+
+    return {
+      ...user,
+      greet
+    };
+  });
+};
+
+const updatedUsers = addGreetingToUsers(users);
+const someUser = getUserById(updatedUsers, 1);
+
+someUser.greet();
+
+/**
+ *  Destructuring / Spread operator
+ *  Loops ( for, for in, for of )
+ *  Functii ES6
+ *  evenimentele (formulare, submit => event.preventDefault())
+ */
